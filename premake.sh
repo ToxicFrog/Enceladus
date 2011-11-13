260c54624b61940d9fb4b1046e144cada2b1e1f5
Ben Kelly <bkelly@uoguelph.ca>
2011-11-13 12:28:38 -0500

    Premake shell script.

---
+++ premake.sh
@@ -0,0 +1,7 @@
+premake4 --os=linux gmake
+premake4 --os=linux codeblocks
+premake4 --os=windows gmake
+premake4 --os=windows codeblocks
+premake4 --os=windows vs2003
+premake4 --os=macosx gmake
+premake4 --os=macosx xcode3
