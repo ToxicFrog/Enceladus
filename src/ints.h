e384eb2f74fdba68b69ed9479b17fc400030af5c
Ben Kelly <bkelly@uoguelph.ca>
2011-11-13 11:42:42 -0500

    Stdint fixes

--- src/ints.h
+++ src/ints.h
@@ -3,12 +3,14 @@
 
 // win32 doesn't have <stdint.h>, so we need to provide our own versions
 // using the nonstandard win32 type names
+// we also need to put an underscore in front of snprintf for some reason
 
-#ifndef WIN32
-    #include <stdint.h>
-#else
+#if defined(WIN32) || defined(_WIN32)
+    #define snprintf _snprintf
     typedef unsigned __int32 uint32_t;
     typedef unsigned __int8 uint8_t;
+#else
+    #include <stdint.h>
 #endif
 
 #endif
